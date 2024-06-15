15675bf4c21599e21d19b18d16c6e84e8c6728e1
HavenDV <havendv@gmail.com>
2024-06-15 17:25:27 +0400

    test: Added Cli integration tests.

---
+++ src/tests/OpenApiGenerator.IntegrationTests.Cli/CliTests.cs
@@ -0,0 +1,68 @@
+﻿using CliWrap.Buffered;
+
+namespace OpenApiGenerator.IntegrationTests;
+
+[TestClass]
+public class CliTests
+{
+    [DataTestMethod]
+    //[DataRow("github.yaml")]
+    [DataRow("ipinfo.yaml")]
+    [DataRow("langsmith.yaml")]
+    //[DataRow("langsmith.json")]
+    [DataRow("ollama.yaml")]
+    [DataRow("openai.yaml")]
+    [DataRow("petstore.yaml")]
+    [DataRow("replicate.json")]
+    //[DataRow("special-cases.yaml")]
+    [DataRow("twitch.json")]
+    public async Task Run(string spec)
+    {
+        var tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        try
+        {
+            Directory.CreateDirectory(tempDirectory);
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var repositoryDirectory = Path.GetFullPath(Path.Combine(currentDirectory, "../../../../../.."));
+            _ = await CliWrap.Cli.Wrap("dotnet")
+                .WithArguments([
+                    "run",
+                    "--project", "src/libs/OpenApiGenerator.Cli",
+                    "generate", $"specs/{spec}",
+                    "--output", tempDirectory,
+                    "--namespace", "Oag",
+                    // --json-serializer-type SystemTextJson --target-framework netstandard2.0 --generate-sdk
+                ])
+                .WithWorkingDirectory(repositoryDirectory)
+                .ExecuteBufferedAsync();
+
+            Directory.EnumerateFiles(tempDirectory, "*", SearchOption.AllDirectories)
+                .Should()
+                .NotBeEmpty();
+
+            await File.WriteAllTextAsync(Path.Combine(tempDirectory, "Oag.csproj"), @"<Project Sdk=""Microsoft.NET.Sdk"">
+
+  <PropertyGroup>
+    <TargetFramework>net8.0</TargetFramework>
+  </PropertyGroup>
+
+  <PropertyGroup Label=""Analyzers"">
+    <EnableNETAnalyzers>true</EnableNETAnalyzers>
+    <AnalysisLevel>latest</AnalysisLevel>
+    <AnalysisMode>All</AnalysisMode>
+  </PropertyGroup>
+
+</Project>");
+            
+            var response = await DotnetCliWrapper.Dotnet.BuildAsync(tempDirectory + "/");
+            Console.WriteLine(response.StandardOutput);
+            Console.WriteLine(response.StandardError);
+            response.Diagnostics.Should().BeEmpty();
+        }
+        finally
+        {
+            Directory.Delete(tempDirectory, recursive: true);
+        }
+    }
+}
