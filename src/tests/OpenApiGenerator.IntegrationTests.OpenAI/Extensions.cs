ea374a0f5a889d3aac4ce997ed10840ea0276fd7
HavenDV <havendv@gmail.com>
2024-06-20 09:29:32 +0400

    test: Added OpenAI integration tests.

---
+++ src/tests/OpenApiGenerator.IntegrationTests.OpenAI/Extensions.cs
@@ -0,0 +1,46 @@
+﻿namespace OpenApiGenerator.IntegrationTests;
+
+internal static class Extensions
+{
+    public static async Task<byte[]> DownloadAsBytesAsync(
+        this Uri uri,
+        CancellationToken cancellationToken = default)
+    {
+        using var client = new HttpClient();
+        
+        return await client.GetByteArrayAsync(uri, cancellationToken).ConfigureAwait(false);
+    }
+    
+    public static string GetMimeType(
+        string path)
+    {
+        // https://platform.openai.com/docs/guides/vision/what-type-of-files-can-i-upload
+        return Path.GetExtension(path.ToUpperInvariant()) switch
+        {
+            ".JPG" => "image/jpeg",
+            ".JPEG" => "image/jpeg",
+            ".WEBP" => "image/webp",
+            ".PNG" => "image/png",
+            ".GIF" => "image/gif",
+            _ => throw new NotSupportedException("Unsupported image format"),
+        };
+    }
+    
+    public static string ToBase64Url(
+        this byte[] bytes,
+        string mimeType)
+    {
+        return $"data:{mimeType};base64,{Convert.ToBase64String(bytes)}";
+    }
+    
+    public static async Task<string> DownloadAsBase64UrlAsync(
+        this Uri uri,
+        string? mimeType = null,
+        CancellationToken cancellationToken = default)
+    {
+        var bytes = await uri.DownloadAsBytesAsync(cancellationToken).ConfigureAwait(false);
+        mimeType ??= GetMimeType(uri.ToString());
+        
+        return bytes.ToBase64Url(mimeType);
+    }
+}
--- src/tests/OpenApiGenerator.IntegrationTests.OpenAI/Tests.cs
+++ src/tests/OpenApiGenerator.IntegrationTests.OpenAI/Tests.cs
@@ -1,4 +1,6 @@
-﻿namespace OpenApiGenerator.IntegrationTests;
+﻿using System.Text.Json;
+
+namespace OpenApiGenerator.IntegrationTests;
 
 [TestClass]
 public class OpenAiTests
@@ -29,48 +31,94 @@ public class OpenAiTests
                 },
                 new ChatCompletionRequestUserMessage
                 {
-                    Content = "What's the weather like today?",
+                    Content = new ChatCompletionRequestMessageContentPart[]
+                    {
+                        new ChatCompletionRequestMessageContentPartText
+                        {
+                            Type = ChatCompletionRequestMessageContentPartTextType.Text,
+                            Text = "What's on this image?",
+                        },
+                        new ChatCompletionRequestMessageContentPartImage
+                        {
+                            Type = ChatCompletionRequestMessageContentPartImageType.ImageUrl,
+                            ImageUrl = new ChatCompletionRequestMessageContentPartImageImageUrl
+                            {
+                                Url = await new Uri("https://raw.githubusercontent.com/srirammanikumar/DogBreedClassifier/master/images/Labrador_retriever_06455.jpg").DownloadAsBase64UrlAsync().ConfigureAwait(false),
+                            },
+                        },
+                    },
                     Role = ChatCompletionRequestUserMessageRole.User,
                 },
             },
-            model: CreateChatCompletionRequestModel.Gpt35Turbo,
+            model: CreateChatCompletionRequestModel.Gpt4o,
             user: "OpenApiGenerator.IntegrationTests.Tests.CreateChatCompletion").ConfigureAwait(false);
         response.Choices.ElementAt(0).Message.Content.Should().NotBeEmpty();
 
-        const string json = @"{
+        Console.WriteLine(response.Choices.ElementAt(0).Message.Content);
+    }
+    
+    [TestMethod]
+    public void CreateChatCompletionRequestSerialization()
+    {
+        var request = new CreateChatCompletionRequest
+        {
+            Messages = new ChatCompletionRequestMessage[]
+            {
+                new ChatCompletionRequestSystemMessage
+                {
+                    Content = "You are a helpful weather assistant.",
+                    Role = ChatCompletionRequestSystemMessageRole.System,
+                },
+                new ChatCompletionRequestUserMessage
+                {
+                    Content = new ChatCompletionRequestMessageContentPart[]
+                    {
+                        new ChatCompletionRequestMessageContentPartText
+                        {
+                            Type = ChatCompletionRequestMessageContentPartTextType.Text,
+                            Text = "What's on this image?",
+                        },
+                        new ChatCompletionRequestMessageContentPartImage
+                        {
+                            Type = ChatCompletionRequestMessageContentPartImageType.ImageUrl,
+                            ImageUrl = new ChatCompletionRequestMessageContentPartImageImageUrl
+                            {
+                                Url = "https://raw.githubusercontent.com/srirammanikumar/DogBreedClassifier/master/images/Labrador_retriever_06455.jpg",
+                            },
+                        },
+                    },
+                    Role = ChatCompletionRequestUserMessageRole.User,
+                },
+            },
+            Model = CreateChatCompletionRequestModel.Gpt35Turbo,
+            User = "OpenApiGenerator.IntegrationTests.Tests.CreateChatCompletion",
+        };
+        var json = JsonSerializer.Serialize(request, SourceGenerationContext.Default.CreateChatCompletionRequest);
+
+        json.Should().Be(@"{
   ""messages"": [
     {
-      ""System"": {
-        ""content"": ""You are a helpful weather assistant."",
-        ""role"": ""system""
-      },
-      ""IsSystem"": true,
-      ""IsUser"": false,
-      ""IsAssistant"": false,
-      ""IsTool"": false,
-      ""IsFunction"": false,
-      ""Object"": {
-        ""content"": ""You are a helpful weather assistant."",
-        ""role"": ""system""
-      }
+      ""content"": ""You are a helpful weather assistant."",
+      ""role"": ""system""
     },
     {
-      ""IsSystem"": false,
-      ""User"": {
-        ""content"": ""What\u0027s the weather like today?"",
-        ""role"": ""user""
-      },
-      ""IsUser"": true,
-      ""IsAssistant"": false,
-      ""IsTool"": false,
-      ""IsFunction"": false,
-      ""Object"": {
-        ""content"": ""What\u0027s the weather like today?"",
-        ""role"": ""user""
-      }
+      ""content"": [
+        {
+          ""type"": ""text"",
+          ""text"": ""What\u0027s on this image?""
+        },
+        {
+          ""type"": ""image_url"",
+          ""image_url"": {
+            ""url"": ""https://raw.githubusercontent.com/srirammanikumar/DogBreedClassifier/master/images/Labrador_retriever_06455.jpg"",
+            ""detail"": ""auto""
+          }
+        }
+      ],
+      ""role"": ""user""
     }
   ],
-  ""model"": 14,
+  ""model"": ""gpt-3.5-turbo"",
   ""frequency_penalty"": 0,
   ""logprobs"": false,
   ""n"": 1,
@@ -78,9 +126,7 @@ public class OpenAiTests
   ""stream"": false,
   ""temperature"": 1,
   ""top_p"": 1,
-  ""parallel_tool_calls"": true,
   ""user"": ""OpenApiGenerator.IntegrationTests.Tests.CreateChatCompletion""
-}";
-        Console.WriteLine(response.Choices.ElementAt(0).Message.Content);
+}");
     }
 }
