5dc37a12f35348b56885b64f2972d49d77895fa4
HavenDV <havendv@gmail.com>
2024-07-14 18:47:15 +0400

    fix: Added missing polyfill.

--- src/libs/OpenApiGenerator.Core/Generation/Sources.Polyfills.cs
+++ src/libs/OpenApiGenerator.Core/Generation/Sources.Polyfills.cs
@@ -52,6 +52,21 @@ namespace {settings.Namespace}
             
             return content.ReadAsStreamAsync();
         }}
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name=""content""></param>
+        /// <param name=""cancellationToken""></param>
+        /// <returns></returns>
+        public static global::System.Threading.Tasks.Task<byte[]> ReadAsByteArrayAsync(
+            this global::System.Net.Http.HttpContent content,
+            global::System.Threading.CancellationToken cancellationToken)
+        {{
+            content = content ?? throw new global::System.ArgumentNullException(nameof(content));
+            
+            return content.ReadAsByteArrayAsync();
+        }}
     }}
 }}
 #endif".RemoveBlankLinesWhereOnlyWhitespaces();
--- src/tests/OpenApiGenerator.SnapshotTests/Snapshots/Ollama/SystemTextJson/_#G.Polyfills.g.verified.cs
+++ src/tests/OpenApiGenerator.SnapshotTests/Snapshots/Ollama/SystemTextJson/_#G.Polyfills.g.verified.cs
@@ -36,6 +36,19 @@ namespace G
             content = content ?? throw new global::System.ArgumentNullException(nameof(content));
             return content.ReadAsStreamAsync();
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static global::System.Threading.Tasks.Task<byte[]> ReadAsByteArrayAsync(
+            this global::System.Net.Http.HttpContent content,
+            global::System.Threading.CancellationToken cancellationToken)
+        {
+            content = content ?? throw new global::System.ArgumentNullException(nameof(content));
+            return content.ReadAsByteArrayAsync();
+        }
     }
 }
 #endif
