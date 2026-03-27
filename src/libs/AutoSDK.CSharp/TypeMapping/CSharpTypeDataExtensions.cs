3be044a5f0d1eb1dc8bfded4a6c84ec3bfdf8b91
Konstantin S. <havendv@gmail.com>
2026-03-27 21:43:25 +0400

    fix: use portable typedata hash combiner

--- src/libs/AutoSDK.CSharp/TypeMapping/CSharpTypeDataExtensions.cs
+++ src/libs/AutoSDK.CSharp/TypeMapping/CSharpTypeDataExtensions.cs
@@ -90,51 +90,64 @@ public static class CSharpTypeDataExtensions
 
     private static int GetDependencyHash(TypeData type, bool isAnyOfLike, string converterType)
     {
-        HashCode hashCode = default;
-        AddOrdinal(ref hashCode, type.CSharpTypeRaw);
-        hashCode.Add(type.CSharpTypeNullability);
-        hashCode.Add(type.IsBaseClass);
-        hashCode.Add(type.IsDerivedClass);
-        hashCode.Add(type.IsArray);
-        hashCode.Add(type.IsNullable);
-        hashCode.Add(type.IsEnum);
-        hashCode.Add(type.IsBase64);
-        hashCode.Add(type.IsDate);
-        hashCode.Add(type.IsDateTime);
-        hashCode.Add(type.IsBinary);
-        hashCode.Add(type.IsValueType);
-        hashCode.Add(type.IsUnixTimestamp);
-        hashCode.Add(type.AnyOfCount);
-        hashCode.Add(type.OneOfCount);
-        hashCode.Add(type.AllOfCount);
-        hashCode.Add(type.IsComponent);
-        hashCode.Add(type.HasDiscriminator);
-        AddOrdinal(ref hashCode, type.Namespace);
-        AddOrdinal(ref hashCode, type.GeneratedNamespace);
-        hashCode.Add(type.IsDeprecated);
-        hashCode.Add(isAnyOfLike);
-        AddOrdinal(ref hashCode, converterType);
+        var hash = 17;
+        AddOrdinal(ref hash, type.CSharpTypeRaw);
+        AddHash(ref hash, type.CSharpTypeNullability);
+        AddHash(ref hash, type.IsBaseClass);
+        AddHash(ref hash, type.IsDerivedClass);
+        AddHash(ref hash, type.IsArray);
+        AddHash(ref hash, type.IsNullable);
+        AddHash(ref hash, type.IsEnum);
+        AddHash(ref hash, type.IsBase64);
+        AddHash(ref hash, type.IsDate);
+        AddHash(ref hash, type.IsDateTime);
+        AddHash(ref hash, type.IsBinary);
+        AddHash(ref hash, type.IsValueType);
+        AddHash(ref hash, type.IsUnixTimestamp);
+        AddHash(ref hash, type.AnyOfCount);
+        AddHash(ref hash, type.OneOfCount);
+        AddHash(ref hash, type.AllOfCount);
+        AddHash(ref hash, type.IsComponent);
+        AddHash(ref hash, type.HasDiscriminator);
+        AddOrdinal(ref hash, type.Namespace);
+        AddOrdinal(ref hash, type.GeneratedNamespace);
+        AddHash(ref hash, type.IsDeprecated);
+        AddHash(ref hash, isAnyOfLike);
+        AddOrdinal(ref hash, converterType);
 
         foreach (var property in type.Properties)
         {
-            AddOrdinal(ref hashCode, property);
+            AddOrdinal(ref hash, property);
         }
 
         foreach (var enumValue in type.EnumValues)
         {
-            AddOrdinal(ref hashCode, enumValue);
+            AddOrdinal(ref hash, enumValue);
         }
 
         foreach (var subType in type.SubTypes)
         {
-            hashCode.Add(subType.Unbox<TypeData>().DependencyHash);
+            AddHash(ref hash, subType.Unbox<TypeData>().DependencyHash);
         }
 
-        return hashCode.ToHashCode();
+        return hash;
     }
 
-    private static void AddOrdinal(ref HashCode hashCode, string? value)
+    private static void AddOrdinal(ref int hash, string? value)
     {
-        hashCode.Add(value is null ? 0 : StringComparer.Ordinal.GetHashCode(value));
+        AddHash(ref hash, value is null ? 0 : StringComparer.Ordinal.GetHashCode(value));
+    }
+
+    private static void AddHash(ref int hash, bool value)
+    {
+        AddHash(ref hash, value ? 1 : 0);
+    }
+
+    private static void AddHash(ref int hash, int value)
+    {
+        unchecked
+        {
+            hash = (hash * 31) + value;
+        }
     }
 }
